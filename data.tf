73a25da48da0fefd5278d569cbd44e8018605657
thamkrabok <31384816+thamkrabok@users.noreply.github.com>
2024-06-11 22:55:14 +0700

    set default ami

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "aws_ami" "amazon_linux" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*"]
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20230418.0-x86_64-ebs"]
   }
 
   filter {
