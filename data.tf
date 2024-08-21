1e82aec2f8cb2d5319bb3f2e87778b8ca42203ad
thamkrabok <31384816+thamkrabok@users.noreply.github.com>
2024-08-21 10:04:37 +0700

    update autoscaling and improve variables passing

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@ data "aws_ami" "amazon_linux" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.4.20240513.0-kernel-6.1-x86_64"]
+    values = ["al2023-ami-2023.5.20240819.0-kernel-6.1-x86_64"]
   }
 
   filter {
